2f84a8cb8b6622bc803460f64e17c0203a7dd6cd
zelshahawy <ziadelshahawygit@gmail.com>
2024-02-11 22:38:09 -0600

    fixed bug, organised classes, added more abstract methods

--- src/bot.py
+++ src/bot.py
@@ -6,73 +6,109 @@ from botbase import BaseBot, SimulateBots
 
 
 class Players(IntEnum):
+    """Enumeration for Player 1 and Player 2."""
     PLAYER1 = 0
     PLAYER2 = 1
 
-class RandomBot(BaseBot):
-    """
-    """
 
+class RandomBot(BaseBot):
+    """Bot that makes random legal moves in a Go game."""
+
+    game: GoStub
     def __init__(self, game: GoStub):
         """
-        """
-        self.game = game
+        Initialize the bot with the game.
 
-    def make_move(self):
+        game: The game the bot will be playing.
         """
-        """
-        move = random.choice(self.game.available_moves)
-        while not self.game.legal_move(move):
-            move = random.choice(self.game.available_moves)
-        self.game.apply_move(move)
+        self._game = game
 
-class Simulation(SimulateBots):
-    """
-    """
-    def __init__(self, game: GoStub, bots_used: list[BaseBot]) -> None:
-        self.game = game
-        self.bot1, self.bot2 = bots_used
-        self.bot1_wins = self.bot2_wins = self.ties = 0
+    def make_move(self) -> None:
+        """
+        Make a random legal move in the game.
+        """
+        move = random.choice(self._game.available_moves)
+        while not self._game.legal_move(move):
+            move = random.choice(self._game.available_moves)
+        self._game.apply_move(move)
 
 
-    def simulate_games(self, num_of_games: int):
+class RandomSimulation(SimulateBots):
+    """Simulates a number of games between two RandomBots."""
+
+    game: GoStub
+    def __init__(self, game: GoStub) -> None:
         """
+        Initialize the simulation with the game.
+
+        game: The game the bots will be playing.
+        """
+        self._game = game
+        self._bot1 = self._bot2 = RandomBot(self._game)
+        self._bot1_wins = self._bot2_wins = self._ties = 0
+
+    def simulate_games(self, num_of_games: int) -> tuple[float, float, float]:
+        """
+        Simulate a number of games and return the win percentages.
+
+        :param num_of_games: The number of games to simulate.
+        :return: A tuple of the win percentages for bot1, bot2, and ties.
         """
         for _ in range(num_of_games):
-            self.game = GoStub(self.game.size, self.game.num_players)
-            while not self.game.done:
-                if self.game.turn == Players.PLAYER1:
-                    self.bot1.make_move()
+            while not self._game.done:
+                if self._game.turn == Players.PLAYER1:
+                    self._bot1.make_move()
                 else:
-                    self.bot2.make_move()
-                self.game.pass_turn()
+                    self._bot2.make_move()
 
-            results = self.game.outcome
-            print(results)
-            if len(results) == 1:
-                result = results[0]
-                if result == 1:
-                    self.bot1_wins += 1
-                elif result == 2:
-                    self.bot2_wins += 1
-            else:
-                self.ties += 1
-        wining_percentage_1 = (self.bot1_wins / num_of_games) * 100
-        wining_percentage_2 = (self.bot2_wins / num_of_games) * 100
-        tie_percentage = (self.ties) / (num_of_games) * 100
+            self.update_results(self._game.outcome)
+            self._game = GoStub(self._game.size, self._game.num_players)
+            self._bot1 = RandomBot(self._game)
+            self._bot2 = RandomBot(self._game)
+        return self.calculate_percentages(num_of_games)
+
+    def update_results(self, results) -> None:
+        """
+        Update the win/tie counts based on the outcome of a game.
+
+        results: The outcome of a game.
+        """
+        if len(results) == 2:
+            self._ties += 1
+        elif results[0] == 1:
+            self._bot1_wins += 1
+        elif results[0] == 2:
+            self._bot2_wins += 1
+
+    def calculate_percentages(self, num_of_games) ->tuple[float, float, float]:
+        """
+        Calculate the win/tie percentages.
+
+        num_of_games: The number of games simulated.
+        return: A tuple of the win percentages for bot1, bot2, and ties.
+        """
+        wining_percentage_1 = (self._bot1_wins / num_of_games) * 100
+        wining_percentage_2 = (self._bot2_wins / num_of_games) * 100
+        tie_percentage = (self._ties) / (num_of_games) * 100
         return (wining_percentage_1, wining_percentage_2, tie_percentage)
 
 
-if __name__ == "__main__":
+def main(num_games):
+    """
+    Run the simulation and print the results.
 
-    if len(sys.argv) != 2:
-        print("Error in the number of arguments. Usage: python3 src/bot.py NUM_GAMES")
-        sys.exit(1)
+    num_games: The number of games to simulate.
+    """
     current_game = GoStub(9, 2)
-    random_simulation = Simulation(current_game, [RandomBot(current_game), RandomBot(current_game)])
-    num_games = int(sys.argv[1])
+    random_simulation = RandomSimulation(current_game)
     player1_win_percentage, player2_win_percentage, ties_percentage = \
         random_simulation.simulate_games(num_games)
     print(f"Player 1 wins: {player1_win_percentage:.2f}%")
     print(f"Player 2 wins: {player2_win_percentage:.2f}%")
     print(f"Ties: {ties_percentage:.2f}%")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Error in the number of arguments. Usage: python3 src/bot.py NUM_GAMES")
+        sys.exit(1)
+    main(int(sys.argv[1]))
--- src/botbase.py
+++ src/botbase.py
@@ -3,20 +3,32 @@ from abc import ABC, abstractmethod
 
 class BaseBot(ABC):
     """
+    Base class for a bot in a Go Game
     """
 
     @abstractmethod
-    def make_move(self):
+    def make_move(self) -> None:
         """
+        perform a move in a game of go
         """
         raise NotImplementedError
 
 class SimulateBots(ABC):
     """
+    Simulate a bot 
     """
 
     @abstractmethod
-    def simulate_games(self, num_of_games:int, bots_used: set[BaseBot]):
+    def simulate_games(self, num_of_games: int) -> tuple[float, float, float]:
         """
         """
         raise NotImplementedError
+    
+    @abstractmethod
+    def update_results(self, results) -> None:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def calculate_percentages(self, num_of_games) ->tuple[float, float, float]:
+        raise NotADirectoryError
+    
