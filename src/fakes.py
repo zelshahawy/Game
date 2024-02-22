4c487f422d3c10d19ffb03342453562638dd2bfa
esslam-ashour <esslamhosam@gmail.com>
2024-02-21 23:49:03 -0600

    improved superko

--- src/fakes.py
+++ src/fakes.py
@@ -186,9 +186,11 @@ class GoFake(GoBase):
         self._consecutive_passes = 0
 
         if self._superko:
-            self._previous_boards: set[BoardGridType] = set()
+            self._previous_boards: set[tuple[tuple[int | None, ...], ...]] = \
+                set()
         else:
-            self._previous_board: BoardGridType | None = None
+            self._previous_board: tuple[tuple[int | None, ...], ...] | None = \
+                None
 
     @property
     def num_moves(self) -> int:
@@ -268,10 +270,11 @@ class GoFake(GoBase):
         if not self.in_bounds(pos):
             raise ValueError("Move is outside bounds of the board")
 
-        resulting_board_tup = tuple(tuple(row) for row in self.simulate_move(pos).grid)
-        resulting_board = self.simulate_move(pos).grid
+        resulting_board = tuple(
+            tuple(row) for row in self.simulate_move(pos).grid
+        )
 
-        if self._superko and resulting_board_tup in self._previous_boards:
+        if self._superko and resulting_board in self._previous_boards:
             return False
         if resulting_board == self._previous_board:
             return False
@@ -288,7 +291,7 @@ class GoFake(GoBase):
         if self._superko:
             self._previous_boards.add(tuple(tuple(row) for row in self.grid))
         else:
-            self._previous_board = self.grid
+            self._previous_board = tuple(tuple(row) for row in self.grid)
 
         if not self.in_bounds(pos):
             raise ValueError("Move is outside bounds of the board")
