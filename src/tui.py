80c7bac7cb80e98de6f4ffc85993e8870f342558
esslam-ashour <esslamhosam@gmail.com>
2024-02-23 01:22:40 -0600

    Added support for milestone 3 command line parameters to TUI

--- src/tui.py
+++ src/tui.py
@@ -1,12 +1,13 @@
 """
-TUI implementation for GoFake
+TUI implementation for Go
 """
 import sys
 import time
 
 from colorama import Fore, Style
+import click
 
-from fakes import GoFake
+from go import Go
 
 PASS_MOVE = (-1, -1)
 
@@ -18,20 +19,21 @@ COLORS = [
     Fore.MAGENTA,
     Fore.CYAN,
     Fore.WHITE,
-    Fore.BLACK
+    Fore.LIGHTBLACK_EX,
+    Fore.LIGHTGREEN_EX
 ]
 
 STONES = {
-    i : COLORS[i] + "●" + Style.RESET_ALL for i in range(8)
+    i : COLORS[i] + "●" + Style.RESET_ALL for i in range(9)
 }
 
 class GoTUI():
     """
     Class for presenting TUI for a game of Go
     """
-    go: GoFake
+    go: Go
 
-    def __init__(self, go: GoFake) -> None:
+    def __init__(self, go: Go) -> None:
         """
         Constructor
 
@@ -64,7 +66,7 @@ class GoTUI():
                 intersection_char = intersection_chars[intersection_type] \
                     [0 if i == 0 else 1 if i < size - 1 else 2]
                 line += intersection_char if intersection is None \
-                    else STONES[intersection]
+                    else STONES[intersection-1]
                 if j < size - 1:
                     line += "─"
                 if j < size:
@@ -82,7 +84,7 @@ class GoTUI():
         scores = self._go.scores()
         print(Fore.WHITE + "Scores:")
         for player, score in scores.items():
-            print(f"{COLORS[player]} + Player {player}: {score} stones")
+            print(f"{COLORS[player-1]} + Player {player}: {score} stones")
 
     def get_move(self) -> tuple[int, int]:
         """
@@ -150,16 +152,25 @@ class GoTUI():
             self.print_board()
         self.end_game()
 
+@click.command()
+@click.option("-n", "--num-players", default=2, help="Number of players")
+@click.option("-s", "--size", default=19, help="Size of the board")
+@click.option("--simple-ko", is_flag=True, help="Use simple ko rule")
+@click.option("--super-ko", is_flag=True, help="Use super ko rule")
+def main(
+    num_players: int = 2,
+    size: int = 19,
+    simple_ko: bool = True,
+    super_ko: bool = False) -> None:
+    """
+    Main function for the TUI
+    """
+    go = Go(size, num_players, super_ko)
+    tui = GoTUI(go)
+    tui.run_game()
 
 if __name__ == "__main__":
-    try:
-        side = sys.argv[1]
-    except IndexError as exc:
-        raise ValueError("Please provide the side length of the board") from exc
-
-    tui = GoTUI(GoFake(int(side), 2))
     print("\033c", end="")
-    # Welcome message
     _ = input(Fore.GREEN + ">>WELCOME TO 碁! PRESS ANY KEY TO START<<\n")
     print("\033c", end="")
-    tui.run_game()
+    main()
