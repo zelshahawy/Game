949f63142425e624d3636e7f55de99722844cb12
root <root@DESKTOP-46PPCRM.localdomain>
2024-02-28 17:24:12 -0600

    Implemented CLI

--- src/gui.py
+++ src/gui.py
@@ -1,25 +1,13 @@
 """
-GUI implementation for Go stub
+GUI implementation for Go
 """
-
 import sys
 from typing import Optional
+import click
 import pygame
-from fakes import GoFake
 from go import Go
 
-
-BOARD_SIZE = int(sys.argv[1])
-
-CELL_SIZE = 700 // BOARD_SIZE
-PLAYER_STONE_RADIUS = CELL_SIZE // 4
-BOARD_PADDING = 100
-WIDTH_DISPLAY = BOARD_SIZE * CELL_SIZE
-
-REFRESH_RATE = 24
-
 ORANGE = (230, 165, 0)
-
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (100, 100, 100)
@@ -30,14 +18,13 @@ CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 PURPLE = (128, 0, 128)
 
-
-
 PLAYER_COLORS = [0, BLACK, WHITE, BLUE, PURPLE, RED, GREEN, MAGENTA, CYAN, GREY]
+
 class GoGUI():
     """
     Graphical version class for GoUI
     """
-    go: GoFake
+    go: Go
     buttons: dict[str, tuple[int, int]]
     screen: pygame.surface.Surface
     clock_timer: pygame.time.Clock
@@ -62,42 +49,55 @@ class GoGUI():
         self.player_colors = { player_int : PLAYER_COLORS[player_int] for\
         player_int in range(1, go.num_players + 1)}
 
-        self.button_pass_rect = pygame.Rect(5, WIDTH_DISPLAY//2, 50, 30)
-        self. button_start_rect = pygame.Rect(WIDTH_DISPLAY//2 - 50,\
-        WIDTH_DISPLAY//2 + 100, 200, 50)
+        self.BOARD_SIZE = go.size
+        self.CELL_SIZE = 700 // self.BOARD_SIZE
+        self.PLAYER_STONE_RADIUS = self.CELL_SIZE // 4
+        self.BOARD_PADDING = 100
+        self.WIDTH_DISPLAY = self.BOARD_SIZE * self.CELL_SIZE
+        self.REFRESH_RATE = 24
+
+        self.button_pass_rect = pygame.Rect(5, self.WIDTH_DISPLAY//2, 50, 30)
+        self. button_start_rect = pygame.Rect(self.WIDTH_DISPLAY//2 - 50,\
+        self.WIDTH_DISPLAY//2 + 100, 200, 50)
 
         self.FONT = pygame.font.SysFont("Arial", 25)
 
         pygame.display.set_caption("GoGUI")
         self.screen = pygame.display.set_mode(
-            (WIDTH_DISPLAY+BOARD_PADDING, WIDTH_DISPLAY+BOARD_PADDING)
+            (self.WIDTH_DISPLAY + self.BOARD_PADDING, self.WIDTH_DISPLAY + \
+            self.BOARD_PADDING)
         )
 
     def display_board(self) -> None:
         """
         Displays the current state of the board in the GUI
-
-        Returns: nothing
         """
-        for i in range(0, BOARD_SIZE):
-            start_hori = (BOARD_PADDING, i * CELL_SIZE + BOARD_PADDING)
-            end_hori = ((BOARD_SIZE - 1) * CELL_SIZE + BOARD_PADDING,\
-                i * CELL_SIZE + BOARD_PADDING)
+
+        for i in range(0, self.BOARD_SIZE):
+            start_hori = (self.BOARD_PADDING, i * self.CELL_SIZE + \
+            self.BOARD_PADDING)
+            end_hori = ((self.BOARD_SIZE - 1) * self.CELL_SIZE + \
+            self.BOARD_PADDING, i * self.CELL_SIZE + self.BOARD_PADDING)
             pygame.draw.aaline(self.screen, GREY, start_hori, end_hori, 3)
 
-            start_vert = (i * CELL_SIZE + BOARD_PADDING, BOARD_PADDING)
-            end_vert = (i * CELL_SIZE + BOARD_PADDING,\
-                (BOARD_SIZE - 1) * CELL_SIZE + BOARD_PADDING)
+            start_vert = (i * self.CELL_SIZE + self.BOARD_PADDING, \
+            self.BOARD_PADDING)
+            end_vert = (i * self.CELL_SIZE + self.BOARD_PADDING,\
+            (self.BOARD_SIZE - 1) * self.CELL_SIZE + self.BOARD_PADDING)
             pygame.draw.aaline(self.screen, GREY, start_vert, end_vert, 3)
 
     def _on_click(self, pos_click: tuple[int, int]) -> None:
         """
-        Handles interactions with the GUI
+        Handles click interactions with the GUI
+            Args: 
+                pos_click (tuple[int, int]) -  the position on the pygame
+                display window where the click occured
         """
 
         if self.button_pass_rect.collidepoint(pos_click):
             self._go.pass_turn()
             return
+
         elif self.button_start_rect.collidepoint(pos_click):
             self. button_start_rect = pygame.Rect(0,0,0,0)
             self.game_started = True
@@ -110,14 +110,20 @@ class GoGUI():
             euclid_sq = (x_click - x_center ) ** 2 + (y_click - y_center) ** 2
             euclid_dist = euclid_sq ** 0.5
 
-            if euclid_dist <= PLAYER_STONE_RADIUS and \
+            if euclid_dist <= self.PLAYER_STONE_RADIUS and \
             self._go.legal_move(board_pos):
                 self._go.apply_move(board_pos)
 
-    def _draw_button(self, font : pygame.font.Font, rect: \
+    def _draw_button(self, font: pygame.font.Font, rect: \
         pygame.rect.Rect, text: str) -> None:
         """
-        Draws the pass button
+        Draws pygame button with specidied font and rectangle params
+            Args:
+                font (pygame.font.Font) - font style on the buttons text
+                rect (pygame.rect.Rect) - rectangle parameters where the button
+                will be positioned in the window display
+                text (str) - the call to action text to be displayed on the
+                button
         """
         pygame.draw.rect(self.screen, WHITE, rect)
 
@@ -133,13 +139,19 @@ class GoGUI():
 
         For empty positions, a circle indicator is shown around the nearest
         line intersection to guide player where to click on next
+
+            Args:
+                num_player (int|None) - the nth player's turn
+                board_pos (tuple[int, int]|None) - the position on the board to
+                draw all stones on the board or an outline of the position one
+                can place the next stone
         """
         if board_pos is None:
             return
         x, y = board_pos
 
-        stone_x = x * CELL_SIZE + BOARD_PADDING
-        stone_y = y * CELL_SIZE + BOARD_PADDING
+        stone_x = x * self.CELL_SIZE + self.BOARD_PADDING
+        stone_y = y * self.CELL_SIZE + self.BOARD_PADDING
 
         self.all_pos[(x,y)] = (stone_x, stone_y)
 
@@ -148,9 +160,9 @@ class GoGUI():
                 self.screen,
                 self.player_colors[num_player],
                 (stone_x, stone_y),
-                PLAYER_STONE_RADIUS)
+                self.PLAYER_STONE_RADIUS)
 
-        elif num_player is None:
+        elif num_player is None and not self._go.done:
             self._hover_board_pos(pygame.mouse.get_pos())
 
     def _hover_board_pos(self, pos_hover: tuple[int,int]) -> None:
@@ -169,29 +181,34 @@ class GoGUI():
             euclid_sq = (x_hover - x_center )**2 + (y_hover - y_center)**2
             euclid_dist = euclid_sq**0.5
 
-            if euclid_dist <= PLAYER_STONE_RADIUS:
+            if euclid_dist <= self.PLAYER_STONE_RADIUS:
 
                 pygame.draw.circle(
                 self.screen,
                 GREY,
                 (x_center, y_center),
-                PLAYER_STONE_RADIUS,
+                self.PLAYER_STONE_RADIUS,
                 width = 1)
 
                 return
 
-    def display_texts(self, text : str, font : pygame.font.Font, pos_text : \
-        tuple[int, int] = (BOARD_SIZE , 30)) -> None:
+    def display_texts(self, text: str, font: pygame.font.Font, pos_text: \
+        tuple[int, int] = (20, 30)) -> None:
         """
         Displays important information such as current player turn, final winner
         and scores
+            Args:
+                text (str) - the text to be displayed
+                font (pygame.font.Font) - font style on the buttons text
+                pos_text (tuple[int, int]) -  the position on the pygame window
+                where the text will be rendered
         """
         img = font.render(text, True, BLACK)
         self.screen.blit(img, pos_text)
 
     def _draw_board_state(self) -> None:
         """
-        Draws all stones on board 
+        Draws all stones on board to represent the current state of the board
         """
         grid_state = self._go.grid
 
@@ -199,10 +216,10 @@ class GoGUI():
             for j, _ in enumerate(grid_state[i]):
                 piece_at_pos = self._go.piece_at((i, j))
                 self._draw_player_stone(piece_at_pos, (i, j))
-
+ 
     def _draw_window(self) -> None:
         """
-        Displays window
+        Displays pygame's interactive window
         """
         self.screen.fill(ORANGE)
 
@@ -219,7 +236,6 @@ class GoGUI():
             else:
                 text = f" Current turn: Player {self._go.turn} " + \
                 f" Game Scores : {self._go.scores()}"
-                self._draw_player_stone(self._go.turn, (-1, -1))
 
             self.display_texts(text,self.FONT)
 
@@ -228,9 +244,8 @@ class GoGUI():
             GO_FONT = pygame.font.SysFont("Arial", 100)
 
             self.display_texts("GO",GO_FONT, \
-            (WIDTH_DISPLAY//2, WIDTH_DISPLAY//2))
+            (self.WIDTH_DISPLAY//2, self.WIDTH_DISPLAY//2))
             self._draw_button(START_FONT, self.button_start_rect,"START GAME")
-
 
     def gui_loop(self) -> None:
         """
@@ -242,6 +257,7 @@ class GoGUI():
             for event in py_events:
                 if event.type == pygame.QUIT:
                     loop = False
+                    pygame.quit()
                     sys.exit()
 
                 elif self._go.done:
@@ -252,11 +268,28 @@ class GoGUI():
 
             self._draw_window()
             pygame.display.update()
-            self.clock_timer.tick(REFRESH_RATE)
-    
-        pygame.quit()
+            self.clock_timer.tick(self.REFRESH_RATE)
 
-def play_sound(sound_path):
+@click.command()
+@click.option("-n", "--num-players", default=2, help="Number of players")
+@click.option("-s", "--size", default=19, help="Size of the board")
+@click.option("--simple-ko", is_flag=True, help="Use simple ko rule")
+@click.option("--super-ko", is_flag=True, help="Use super ko rule")
+def create_game(num_players: int , size: int, simple_ko : bool, super_ko: bool):
+    """
+    Creates go game from click commands, initializes GUI
+        Args:
+            num_players (int) -  the number of players in go
+            size (int) -  size of go board to be used
+            simple_ko (bool) - property that sets the simple_ko rule if super_ko
+            is not declared in click interface
+            super_ko - property that sets super_ko rule
+    """
+    go = Go(size, num_players, super_ko)
+    goGUI = GoGUI(go)
+    goGUI.gui_loop()
+
+def play_sound(sound_path : any) -> None:
     """
     given a path to a an audio file, plays it.
     """
@@ -268,6 +301,4 @@ def play_sound(sound_path):
 
 if __name__ == "__main__":
     play_sound("src/data/bgm")
-    go = Go(19,9)
-    goGUI = GoGUI(go)
-    goGUI.gui_loop()
+    create_game()
